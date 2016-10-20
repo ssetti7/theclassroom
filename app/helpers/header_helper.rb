68e597c8c7835e2347704385d1be0c1d3ab95326
lissett <lissettfundora@gmail.com>
2016-10-20 15:55:57 -0400

    setting up menu

--- app/helpers/header_helper.rb
+++ app/helpers/header_helper.rb
@@ -1,13 +1,13 @@
 module HeaderHelper
 
   def render_top_view
-    return top_link unless current_user
-    render partial: 'layouts/partial-general/header_user'
+    return login_link unless current_user
+    render partial: 'layouts/partial-general/header_menu'
   end
 
   private
 
-  def top_link
+  def login_link
     link_to 'Log in', new_user_session_path
   end
 end
